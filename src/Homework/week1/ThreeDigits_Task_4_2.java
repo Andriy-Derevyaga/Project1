122be33a4fff5f096a6cdfbe3dcedffe5c8a6e2b
andriy.derevyaga <andriy.derevyaga@worldapp.com>
2017-10-13 13:51:26 +0300

    src add

---
+++ src/Homework/week1/ThreeDigits_Task_4_2.java
@@ -0,0 +1,27 @@
+package Homework.week1;
+
+import java.util.Scanner;
+
+public class ThreeDigits_Task_4_2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first integer value");
+        int A = sc.nextInt();
+        System.out.println("Enter first integer value");
+        int B = sc.nextInt();
+        System.out.println("Enter first integer value");
+        int C = sc.nextInt();
+
+        int BiggestNumber();
+        int SmallestNumber();
+
+        if (A=B && A=C && B=C) ;
+
+
+
+    }
+
+}
