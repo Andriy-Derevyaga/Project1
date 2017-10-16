5a074f606b87dc124acff6f49d77a83d541e4a63
Andriy.Derevyaga <andriy.derevyaga@gmail.com>
2017-10-16 18:23:37 +0300

    Task_4_2_added

--- src/Homework/week1/ThreeDigits_Task_4_2.java
+++ src/Homework/week1/ThreeDigits_Task_4_2.java
@@ -1,3 +1,4 @@
+
 package Homework.week1;
 
 import java.util.Scanner;
@@ -9,19 +10,48 @@ public class ThreeDigits_Task_4_2 {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter first integer value");
-        int A = sc.nextInt();
-        System.out.println("Enter first integer value");
-        int B = sc.nextInt();
-        System.out.println("Enter first integer value");
-        int C = sc.nextInt();
+        int a = sc.nextInt();
+        System.out.println("Enter second integer value");
+        int b = sc.nextInt();
+        System.out.println("Enter third integer value");
+        int c = sc.nextInt();
 
-        int BiggestNumber();
-        int SmallestNumber();
+        int BiggestNumber;
+        int SmallestNumber;
 
-        if (A=B && A=C && B=C) ;
-
-
-
+        if (a == b && a == c && b == c) {
+            System.out.println("All digits are the same");
+            }
+        else {
+            if (a > b)
+                { if (b > c) {
+                BiggestNumber = a;
+                SmallestNumber = c;
+                System.out.println("The biggest number is " + BiggestNumber);
+                System.out.println("The smallest number is " + SmallestNumber);
+                }
+                    else {
+                        BiggestNumber = a;
+                        SmallestNumber = b;
+                        System.out.println("The biggest number is " + BiggestNumber);
+                        System.out.println("The smallest number is " + SmallestNumber);
+                        }}
+            else { if (b > c) {
+                    BiggestNumber = b;
+                    SmallestNumber = c;
+                    System.out.println("The biggest number is " + BiggestNumber);
+                    System.out.println("The smallest number is " + SmallestNumber);
+                            }
+                    else {
+                    BiggestNumber = c;
+                    SmallestNumber = a;
+                    System.out.println("The biggest number is " + BiggestNumber);
+                    System.out.println("The smallest number is " + SmallestNumber);
+                        }
+                }
+            }
+        }
     }
 
-}
+
+
